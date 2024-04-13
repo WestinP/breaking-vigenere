a98b9c7f665485d0e0ea82ee8b6b38f5eece012f
Micah Parrilla <micahparrilla@icloud.com>
2024-04-13 13:15:33 -0400

    Added IOC & Period calculation. Tested with a longer plaintext.

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -34,10 +34,46 @@ def decrypt_vigenere_cipher(L, key, text):
 def vigenere_cipher_frequency_attack(L, text):
     return 0
 
+def index_of_coincidence(text):
+    frequency = [0]*26 # frequency of each letter in the alphabet
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    total = 0
+    for char in text:
+        if char in alphabet:
+            frequency[alphabet.index(char)] += 1 # increment the frequency of the letter
+            total += 1
+    ic = 0 
+    numerator = 0
+    for i in range(26):
+        numerator += frequency[i] * (frequency[i] - 1)  # Sum the numerators
+    if total > 1:  # Avoid division by zero
+        ic = 26 * numerator / (total * (total - 1))
+    return ic 
+
+def index_of_coincidence_period(text):  
+    found = False
+    period = 0
+    while not found:
+        period += 1
+        slices = [''] * period 
+        for i in range(len(text)):
+            slices[i % period] += text[i] # Split the text into slices
+        sum = 0
+        for i in range(period):
+            sum += index_of_coincidence(slices[i]) # Sum the index of coincidences of each slice
+        index_of_coincidence_average = sum / period 
+        if index_of_coincidence_average > 1.6: # If the average index of coincidence is greater than 1.6, the period is found
+            found = True
+            return period
 
 # Testing the function
 L = 3
 key = 'key'
-text = 'hello worlD'
-print(vigenere_cipher(L, key, text))  # prints 'rijvs gspvh'
-print(decrypt_vigenere_cipher(L, key, 'rijvs gspvh'))  # prints 'hello world'
+#changed the text to be longer since the index of coincidence is more accurate with a larger text than a short one
+text = 'hello world i am westin pellicer and i am a student at the university of embry riddle i like fortnite and travvy patty mealz'
+cipherText = vigenere_cipher(L, key, text)
+iocPeriod = index_of_coincidence_period(cipherText)
+print(cipherText)  # prints 'rijvs gspvh'
+print(decrypt_vigenere_cipher(L, key, cipherText))  # prints 'hello world' decrypted from cipherText
+print(index_of_coincidence(cipherText)) # p
+print(index_of_coincidence_period(cipherText)) #Should return 3
