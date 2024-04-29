68763b2f629b20e463637e65729830453cd15a19
Jack Morris <jack@jackmorris.me>
2024-04-29 16:16:58 -0400

    fixed bruteforce runtime

--- is_english_word.py
+++ is_english_word.py
@@ -1,15 +1,18 @@
+filereader = open('dictionary.txt', 'r')
+dictionary = filereader.readlines()
+filereader.close()
+
+real_words = {}
+
+for word in dictionary:
+    word = word.strip()
+    word = word.lower()
+    real_words[word] = 1
+
 def is_logically_english(text: str, percent: float) -> bool:
     maybe_words = text.split()
     if len(maybe_words) == 0:
         return False
-    real_words = {}
-    filereader = open('dictionary.txt', 'r')
-    dictionary = filereader.readlines()
-    filereader.close()
-    for word in dictionary:
-        word = word.strip()
-        word = word.lower()
-        real_words[word] = 1
 
     count = 0
     for word in maybe_words:
@@ -20,4 +23,3 @@ def is_logically_english(text: str, percent: float) -> bool:
     return False
 
 
-print(is_logically_english('hello adsasdasdwdasd', 0.90))
